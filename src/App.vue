8f4a1609cc415c21dc768e72f7d78348adbbe6e5
den-silva <dmurilosilva@hotmail.com>
2021-08-20 13:47:04 -0300

    Criação do projeto, cabeçalho eu rodapé da página

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <cabecalho/>
+    <rodape/>
+  </div>
+</template>
+
+<script>
+  import cabecalho from './components/cabecalho.vue'
+import Rodape from './components/rodape.vue'
+  import rodape from './components/rodape.vue'
+  export default {
+    name: 'App',
+    components: {
+      cabecalho,
+      rodape
+    }
+  }
+
+        Rodape</script>
+
+<style>
+
+</style>
