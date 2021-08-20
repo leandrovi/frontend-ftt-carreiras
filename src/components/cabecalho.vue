3a5a2c98286a36c239257c6408009d45f39f7bc4
den-silva <dmurilosilva@hotmail.com>
2021-08-20 15:08:20 -0300

    Primeiro template de cabeçalho e rodapé

--- src/components/cabecalho.vue
+++ src/components/cabecalho.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
-        
+        <header>
+            <div id="main-header">
+                <a href="/"><img src="../assets/cefsaLogo.png" alt=""></a>
+                <nav>
+                    <ul class="menu">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Empresas</a></li>
+                        <li><a href="/">Sobre</a></li>
+                    </ul>
+                </nav>
+            </div>
+            
+        </header>
     </div>
 </template>
 
@@ -12,5 +24,56 @@ export default ({
 </script>
 
 <style scoped>
+    body, ul, li, p{
+        font-family: arial;
+        margin: 0px;
+        padding: 0px;
+        list-style: none;
+    }
 
+    header{
+        width:100%;
+        background: rgb(0, 68, 137);
+
+    }
+    #main-header{
+        display: flex;
+        justify-content: space-between;
+        height:10vh;
+        margin: 0 auto;
+        padding: 10px;
+        align-items: center;
+        flex-wrap: wrap;
+        max-width: 1280px;
+    }
+    
+
+  
+    a{
+        text-decoration: none;
+        color: white;
+        font-family: arial;
+
+    }
+
+    ul{
+
+    }
+    .menu{
+        display: flex;
+    }
+
+    .menu li{
+        margin-left: 10px;
+    }
+
+    .menu li a{
+        display: block;
+        padding: 15px;
+    }
+
+    .menu li a:hover{
+        background-color: rgb(30,98,167);
+
+    }
 </style>
--- src/components/rodape.vue
+++ src/components/rodape.vue
@@ -1,16 +1,69 @@
+
 <template>
     <div>
-        
+        <footer>
+            <div id="main-footer">
+                <h2>FTT Carreiras &copy;2021</h2>  
+                <ul class="menu">
+                    <li><a href="/"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                    <li><a href="/"><ion-icon name="logo-instagram"></ion-icon></a></li>
+                    <li><a href="/"><h2>Contato (11)4359-6565</h2></a></li>
+                </ul>
+            </div>
+            
+
+        </footer>
     </div>
 </template>
+    <script src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
 <script>
-
+    
 export default ({
     
 })
 </script>
 
 <style scoped>
+body, ul, li, p{
+        font-family: arial;
+        margin: 0px;
+        padding: 0px;
+        list-style: none;
+    }
+    footer{
+        width:100%;
+        background: rgb(0, 68, 137);
+        position: absolute;
+        bottom:0;
+    }
+    #main-footer{
+        display: flex;
+        justify-content: space-between;
+        max-width: 1280px;        
+        height:10vh;
+        margin: 0 auto;
+        padding: 15px;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+    
+    a{
+        text-decoration: none;
+        color: white;
+        font-family: arial;
 
+    }
+
+    h2{
+        font-family: arial;
+        color: white;
+    }
+
+    ul{
+
+    }
+    #menu{
+
+    }
 </style>
