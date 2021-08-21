b288d06066ebaf12a413f7b84d91670405cbcdbb
Maria Gabriela <mgmakamine@gmail.com>
2021-08-21 16:24:59 -0300

    Update componentes

--- src/components/Jobs.vue
+++ src/components/Jobs.vue
@@ -1,9 +1,121 @@
 <template>
-  <div></div>
+  <div>
+    <section class="container">
+      <div class="row">
+        <div class="col-12">
+          <h2>Painel de <span style="color: #72b73b;">vagas</span></h2>
+
+          <div class="conteudo">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book
+            </p>
+
+            <a href="">email@email.com</a>
+
+            <a href="">https://processoseletivonestle.com.br/</a>
+
+            <a href="">Estagio</a>
+          </div>
+
+          <div class="conteudo">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book Lorem Ipsum is
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled
+              it to make a type specimen book
+            </p>
+
+            <a href="">email@email.com</a>
+
+            <a href="">https://processoseletivonestle.com.br/</a>
+
+            <a href="">CLT</a>
+          </div>
+
+          <div class="conteudo">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s
+            </p>
+
+            <a href="">email@email.com</a>
+
+            <a href="">https://processoseletivonestle.com.br/</a>
+
+            <a href="">CLT</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+h2 {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 45px;
+}
+
+p {
+  color: #757575;
+  padding: 40px 4px 0px 25px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 26px;
+}
+
+.conteudo {
+  background: #f2f2f2;
+  justify-content: center;
+  padding-bottom: 30px;
+  margin-bottom: 30px;
+}
+
+.conteudo p {
+  padding: 40px 4px 10px 25px;
+}
+
+.conteudo a {
+  color: #757575;
+  padding: 0px 4px 55px 25px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 26px;
+  margin-left: 12%;
+  text-decoration: none;
+}
+
+.container {
+  width: 90%;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.row {
+  display: flex;
+}
+
+.col-12 {
+  width: 100%;
+}
+
+.col-6 {
+  width: 100%;
+}
+</style>
